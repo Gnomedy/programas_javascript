a0e98f9f1b40787416e19074a0b970f0135d34fe
Arthur de Souza Matos <103154624+arthurdesouzamatos@users.noreply.github.com>
2023-08-02 20:17:48 -0300

    Add files via upload

---
+++ assignments.js
@@ -0,0 +1,17 @@
+continent = "América do Sul"
+country = "Brasil";
+population = 200;
+isIsland = false;
+language = "Português";
+
+console.log("Continente: " + continent);
+console.log("País: " + country);
+console.log("Número de habitantes: " + population + " milhões");
+
+if (isIsland == false) {
+    console.log("Não é uma ilha!");
+} else {
+    console.log("É uma ilha!")
+}
+
+console.log("O idioma é: " + language);
